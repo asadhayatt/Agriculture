602a4296ee1bbdc23f38d899d53c232f460cba7f
Muhammad Asad Hayat <asadhkan@gmail.com>
2024-07-14 02:32:07 +0500

    testttt comit

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::prefix('/admin')->group(function () {
     Route::get('/logout',function(){
         Auth::logout();
         return redirect()->to('/login');
-    });
+    });sasd
 
 
     // Admin Pages Routes
@@ -61,3 +61,4 @@ Route::prefix('/admin')->group(function () {
         Route::get('/dashboard',[AdminController::class,'index']);
     });
 });
+s
